ddc0a562ac737e7331fcc8fa9635354cabe16eae
Marcin Drozd <marcindrozd@me.com>
2015-10-01 19:45:16 +0200

    update users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,15 +1,13 @@
 class Admin::UsersController < Admin::BaseController
+  load_and_authorize_resource
 
   def index
-    @users = User.all
   end
 
   def new
-    @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
     if @user.save
       redirect_to edit_admin_user_path(@user)
     else
@@ -18,11 +16,9 @@ class Admin::UsersController < Admin::BaseController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to edit_admin_user_path(@user)
     else
