9a8c4295b7765351563445321e678a53663132f1
Marcin Drozd <marcindrozd@me.com>
2015-10-01 19:14:32 +0200

    add exams views

--- app/controllers/admin/exams_controller.rb
+++ app/controllers/admin/exams_controller.rb
@@ -7,6 +7,28 @@ class Admin::ExamsController < Admin::BaseController
   def new
   end
 
+  def create
+    if @exam.save
+      redirect_to admin_exams_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @exam.update(exam_params)
+      redirect_to edit_admin_exam_path(exam)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
   private
 
   def exam_params
