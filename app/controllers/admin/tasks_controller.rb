e1bdd737a4abe2e982bdd3b0df6f48493f2dcb1c
Marcin Drozd <marcindrozd@me.com>
2015-10-03 12:53:17 +0200

    add task new, edit and destroy actions

--- app/controllers/admin/tasks_controller.rb
+++ app/controllers/admin/tasks_controller.rb
@@ -3,4 +3,37 @@ class Admin::TasksController < Admin::BaseController
 
   def index
   end
+
+  def new
+  end
+
+  def create
+    if @task.save
+      redirect_to admin_edit_task_path(@task)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to admin_edit_task_path(@task)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to admin_tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:number, :level, :skill, subtasks_attributes: [:id, :name, :max_points, :_destroy])
+  end
 end
