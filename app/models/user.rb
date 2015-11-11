f0eab43c98e5caed8b0747770f19d5b3747f2b96
Marcin Drozd <marcindrozd@me.com>
2015-11-11 13:41:22 +0100

    udpate devise and sign in form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,30 @@
 class User < ActiveRecord::Base
+  attr_accessor :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:login]
 
   ROLES = %w(admin teacher)
 
   has_and_belongs_to_many :groups
 
-  validates :email, :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9_\.]*\z/ }
   validates :first_name, :last_name, presence: true
 
   def has_role?(role)
     self.role == role.to_s
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      conditions[:email].downcase! if conditions[:email]
+      where(conditions.to_hash).first
+    end
   end
 end
