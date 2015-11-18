0c33737354f5697aeff29a18b517ab765073ee98
Marcin Drozd <marcindrozd@me.com>
2015-11-18 13:38:00 +0100

    rescue from cancan exception

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -3,4 +3,9 @@ class BaseController < ApplicationController
   include Filterable
 
   before_action :authenticate_user!
+
+  rescue_from CanCan::AccessDenied do |exception|
+    sign_out
+    redirect_to new_user_session_url, alert: t('not_authorized')
+  end
 end
