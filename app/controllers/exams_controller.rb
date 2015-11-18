8d24a81b0ac35b7461d344ab8eadd5c88e3c844b
Marcin Drozd <marcindrozd@me.com>
2015-11-18 13:05:41 +0100

    updates for missing translations

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -3,6 +3,6 @@ class ExamsController < BaseController
 
   def set_current
     set_current_exam(@exam.id)
-    redirect_to root_path
+    redirect_to root_path, notice: t('custom_messages.exam_changed', exam: @exam.name)
   end
 end
