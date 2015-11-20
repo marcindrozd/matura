fbb584218f274c136ff23e459f9e2f4c0908dccc
Marcin Drozd <marcindrozd@me.com>
2015-11-20 15:01:29 +0100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+password = 'teacher123'
+
+User.create(
+    first_name: 'Marcin',
+    last_name: 'Drozd',
+    username: 'marcin',
+    password: password,
+    password_confirmation: password,
+    email: 'marcin@example.com',
+    role: 'admin'
+  )
+
+User.create(
+    first_name: 'Ania',
+    last_name: 'Drozd',
+    username: 'ania',
+    password: password,
+    password_confirmation: password,
+    email: 'ania@example.com',
+    role: 'admin'
+  )
