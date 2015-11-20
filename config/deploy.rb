112689d21d1684310ba7383097a9f39ba3df7d42
Marcin Drozd <marcindrozd@me.com>
2015-11-20 19:19:14 +0100

    config

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,6 @@ set :deploy_to, '/home/deploy/matura'
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-ssh_options[:forward_agent] = true
-
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
