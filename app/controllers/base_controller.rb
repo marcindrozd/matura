f00e7d2e89eac9a2a9d0bda2f0683b5e31650204
Marcin Drozd <marcindrozd@me.com>
2015-12-28 08:48:06 +0100

    hide admin menu from regular users

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -5,8 +5,7 @@ class BaseController < ApplicationController
   before_action :authenticate_user!
 
   rescue_from CanCan::AccessDenied do |exception|
-    sign_out
-    redirect_to new_user_session_url, alert: t('not_authorized')
+    redirect_to exam_groups_url, alert: t('not_authorized')
   end
 
   private
