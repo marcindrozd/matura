5d58527e562635436add76e803ac20d15bac10d8
Marcin Drozd <marcindrozd@me.com>
2015-12-28 23:18:24 +0100

    start skills with capital letter

--- app/helpers/table_helper.rb
+++ app/helpers/table_helper.rb
@@ -2,7 +2,7 @@ module TableHelper
 
   def skills_heading(tasks)
     tasks.map { |t| t.skill }.uniq.each do |skill|
-      concat content_tag(:th, skill, colspan: "#{tasks.where(skill: skill).count}")
+      concat content_tag(:th, skill.capitalize, colspan: "#{tasks.where(skill: skill).count}")
     end
   end
 end
