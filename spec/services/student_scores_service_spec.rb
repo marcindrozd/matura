3b89149c4915a51784269a588814ed2d2379405d
Marcin Drozd <marcindrozd@me.com>
2016-02-10 19:12:49 +0100

    update specs

--- spec/services/student_scores_service_spec.rb
+++ spec/services/student_scores_service_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
-describe StudentScoresHandler do
+describe StudentService do
   let!(:exam) { create :exam }
-  let!(:task) { create :task, exam: exam }
+  let!(:group) { create :group, exam: exam, kind: 'rozszerzona' }
+  let!(:task) { create :task, exam: exam, level: 'standard' }
   let!(:subtask1) { create :subtask, task: task }
   let!(:subtask2) { create :subtask, task: task }
 
   it 'assigns blank scores for all available exam tasks when new student is created' do
-    student = create :student
+    student = create :student, group: group
 
-    service = StudentScoresHandler.new(exam, student)
-    service.add_all_tasks_to_student
+    service = StudentService.new(student)
+    service.create('standard')
 
     expect(student.reload.scores.count).to eq(2)
   end
--- spec/services/task_service_spec.rb
+++ spec/services/task_service_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-describe TaskScoresHandler do
+describe TaskService do
   let!(:exam) { create :exam }
   let!(:group) { create :group, exam: exam }
-  let!(:student1) { create :student, group: group }
-  let!(:student2) { create :student, group: group }
+  let!(:student1) { create :student, group: group, level: 'standard' }
+  let!(:student2) { create :student, group: group, level: 'standard' }
 
   it 'adds newly created task to all existing students' do
-    task = create :task, exam: exam
+    task = create :task, exam: exam, level: 'standard'
     subtask1 = create :subtask, task: task
     subtask2 = create :subtask, task: task
 
-    service = TaskScoresHandler.new(exam, task)
-    service.add_task_to_all_students
+    service = TaskService.new(task)
+    service.create
 
     expect(student1.reload.scores.count).to eq(2)
     expect(student2.reload.scores.count).to eq(2)
